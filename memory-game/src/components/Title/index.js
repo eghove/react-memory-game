df798ee9eb17791828155e18e4cacfdb016648d7
That_Guy <eghove@gmail.com>
2019-02-09 07:18:18 -0600

    building base Title component index.js

---
+++ memory-game/src/components/Title/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+// Title component index.js file
+
+import React from 'react';
+import './style.css';
+
+function Title (props) {
+  return <h1 className = 'title'>{props.children}</h1>
+}
+
+export default Title;
