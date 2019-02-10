bdd0d0b3ab3fa90b7500532308edf953933fc94f
That_Guy <eghove@gmail.com>
2019-02-10 16:02:39 -0600

    got a randomized function to work to shuffle the memory cards

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -18,6 +18,36 @@ class App extends Component {
     spaceObjects
   };
 
+
+  // the function that randomizes things on the page
+  handleRandomize = () => {
+    let temp = this.state.spaceObjects
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      temp = array;
+    }
+    shuffle(temp);
+    this.setState(temp);
+  }
+
+  componentDidMount() {
+    // makes sure it start from a randomized state
+    this.handleRandomize();
+  }
+  
   render() {
     return (
       <Wrapper>
@@ -32,18 +62,19 @@ class App extends Component {
         {/* the Title Component */}
         <Title
           title='Memory Game'
-          message='A simple click-based memory game.' 
+          message='A simple click-based memory game.'
         />
-          
+
         {/* the Memory card display component */}
         <div className="row">
-            {this.state.spaceObjects.map(spaceObject => (
-              <MemoryCard
-                id={spaceObject.id}
-                name={spaceObject.name}
-                image={spaceObject.image}
-              />
-            ))}
+          {this.state.spaceObjects.map(spaceObject => (
+            <MemoryCard
+              key = {spaceObject.id}
+              id={spaceObject.id}
+              name={spaceObject.name}
+              image={spaceObject.image}
+            />
+          ))}
         </div>
 
 
