e8f782a02a6156000226039d95c8e46c99e2f148
That_Guy <eghove@gmail.com>
2019-02-10 13:22:55 -0600

    got all of the images in cards

--- memory-game/src/components/MemoryCard/index.js
+++ memory-game/src/components/MemoryCard/index.js
@@ -1,12 +1,23 @@
 import React from "react";
+import "./index.css";
 
 function MemoryCard (props) {
   return (
+    <div className = "row">
+    <div className = "col-sm-4">
     <div className = "card" data-id = {props.id}>
-      <div className = "img-container">
-        <img alt = {props.name} src = {props.image} />
-      </div>
+    <div className = "card-body img-container">
+    <img alt = {props.name} src = {props.image} />
+    
     </div>
+    </div>
+    </div>
+    </div>
+    // <div className = "card" data-id = {props.id}>
+    //   <div className = "img-container">
+    //     <img alt = {props.name} src = {props.image} />
+    //   </div>
+    // </div>
   )
 }
 
