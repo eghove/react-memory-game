3e9284d46ccfdb161e71914278dada7b45ca7ee3
That_Guy <eghove@gmail.com>
2019-03-03 14:32:46 -0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-memory-game
-This is a simple memory game using React.js
---
+++ precache-manifest.53f72e87b507caeecb4773159f8553df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "9ba1e8bee97909ed5ba8",
+    "url": "/react-memory-game/static/js/main.9ba1e8be.chunk.js"
+  },
+  {
+    "revision": "272c1fe465e25c53ac6a",
+    "url": "/react-memory-game/static/js/1.272c1fe4.chunk.js"
+  },
+  {
+    "revision": "9ba1e8bee97909ed5ba8",
+    "url": "/react-memory-game/static/css/main.9d100360.chunk.css"
+  },
+  {
+    "revision": "5f14b1da2e06c2ecb607ef134c1249ee",
+    "url": "/react-memory-game/index.html"
+  }
+];
